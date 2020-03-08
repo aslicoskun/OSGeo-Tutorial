ea187e9650f62abc1297af7e883a684f5a006d6b
aslicoskun <39247570+aslicoskun@users.noreply.github.com>
2020-03-08 15:44:55 +0300

    Add files via upload

---
+++ appConfig.js
@@ -0,0 +1,48 @@
+var developmentDatabase = {
+    postgres: {
+    host: 'localhost',
+    port: 5432,
+    database: 'database_name',
+    user: 'postgres',
+    password: 'postgres'
+    }
+    }
+    
+    var connectionString = "postgres://mattccsicizawu:b97e7299415baef4b85d4664ee125960456eede807e16b5754dad662b5db1e47@ec2-50-17-178-87.compute-1.amazonaws.com:5432/d8dj862atr6cqn";
+    if (process.env.NODE_ENV == 'production') {
+    //Production mode
+    if (process.env.DATABASE_URL) {
+    developmentDatabase =
+    parseConnectionString(process.env.DATABASE_URL);
+    } else {
+    console.log("process.env.DATABASE_URL empty, connectionStringvariable used");
+    developmentDatabase = parseConnectionString(connectionString);
+    }
+    }else{
+    //Development mode
+    developmentDatabase = parseConnectionString(connectionString);
+    }
+    function parseConnectionString(connectionString) {
+    if (connectionString) {
+    var myRegexp = /(\w+):(\w+)@(.+):(\w+)\/(\w+)/g;
+    var match = myRegexp.exec(connectionString);
+    if (match.length == 6) {
+    developmentDatabase.postgres.user = match[1];
+    developmentDatabase.postgres.password = match[2];
+    developmentDatabase.postgres.host = match[3];
+    developmentDatabase.postgres.port = Number(match[4]);
+    developmentDatabase.postgres.database = match[5];
+    developmentDatabase.postgres.ssl = true;
+    return developmentDatabase;
+    }
+    }
+    console.log("connectionString cannot be parsed");
+    return null;
+    }
+    module.exports = {
+    hostname: "http://localhost",
+    port: 5656,
+    database: {
+    postgres: developmentDatabase.postgres
+    }
+    }
